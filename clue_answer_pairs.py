1ff55d514ef2965092b0716cd98e48f128fc31b1
Rachel Prisock <rachelprisock@gmail.com>
2016-05-22 01:32:22 -0400

    Created updated file structure for ability to pull clue, answer, and clue-answer lists separately

--- clue_answer_pairs.py
+++ clue_answer_pairs.py
@@ -1,73 +1,19 @@
-from bs4 import BeautifulSoup
-import requests
-import xml.etree.ElementTree
-import pdb
+## This file creates a csv of clue answer pairs and a count of
+## how many times the clue appeared with all possible answers
 
-# This block sets the stage for getting a list of links to the different years of crossword puzzles
-home = requests.get('http://www.xwordinfo.com/xml/Crossword/').content
-soup = BeautifulSoup(home, 'html.parser')
-home_links = soup.find_all('a')
-year_links = []
-month_links = []
-xml_links = []
-clue_list = []
-answer_list = []
-clue_answer_pairs = {}
-
-# This block gets a list called year_links with includes all links to different years of crossword puzzles
-print('Init home links \n')
-for link in home_links:
-    link = link.get('href')
-    if link.startswith('/xml/Crossword/'):
-        link = 'http://www.xwordinfo.com' + link
-        year_links.append(link)
-
-# This block iterates through year_links and creates a list of all months links to crossword puzzles
-print('Init year links')
-for year in year_links:
-    year_page = requests.get(year).content
-    soup2 = BeautifulSoup(year_page, 'html.parser')
-    month_links.append(soup2.find_all('a'))
-    # This gives multiple arrays within an array
-
-# This block iterates through lists month_links and creates a new list of all links to xml files to be parsed through
-print('Init month links')
-for set in month_links:
-    for month in set:
-        month = month.get('href')
-        if month.startswith('/xml/Crossword/' + str(1)):
-            month = 'http://www.xwordinfo.com' + month
-            xml_links.append(month)
-        elif month.startswith('/xml/Crossword/' + str(2)):
-            month = 'http://www.xwordinfo.com' + month
-            xml_links.append(month)
-        else:
-            print('Not writing parent directory link')
-
-# This block iterates through the xml_links list and populates a
-# dictionary of clue_answer_pairs from all years and all months
-print('Init xml links')
-for url in xml_links:
-    link_content = requests.get(url).content
-    tree = xml.etree.ElementTree.fromstring(link_content)
-    for child in tree:
-        for child in child:
-            if child.tag == 'Clues':
-                for child in child:
-                    clue = child.text
-                    ans = child.get('Ans')
-                    clue_answer_pairs[clue] = ans
-
-
-# This file parses the xml links on xwordinfo to gather a dictionary of clue answer pairs
-# To Do:
-    # When a clue appears multiple times, need to add a column to enumerate
-        # the number of occurances of that clue
-    # If a clue appears multiple times, add answer to array corresponding to clue
-    # ex. [[{ apple: [ red, aday, worm]}], [3]]
-        # clue_answer_table = [[{ clue: [ans, ans, ans, ans]}], [times_clue_appeared]]
-    # make into useful methods to set up for creating answer table and clue table
-    # refactor xml_links loop, messy structure.
+### To Do:
+    # Add answers into an array
+    # If clue appears multiple times:
+        # add new answer to answer array corresponding to clue (key)
+        # Add new column in table to enumerate number of times clue appears
+            # Could use len(answer_array) or something to populate that column.
+            # Ex. # clue_answer_table = [[{ clue: [ans, ans, ans, ans]}], [len(answer_array)]]
+        # Print final list to csv file.
+        # Refactor first loop, quite messy
+        # Take into account if a particular clue answer pair that is exactly the same appears
+            # should also probably create a column to record the number of times this happens
+            # but that will have to lie within the answer array potentially
+            # ex. clue_answer_pair_table = [[{ clue: [ans[1], ans[5], ans, ans]}], [len(answer_array)]]
 
 ######### One File Test #########
 # Trying out xml parsing with one file
@@ -84,3 +30,24 @@ for url in xml_links:
 #                 ans = child.get('Ans')
 #                 clue_answer_pairs[clue] = ans
 # print(clue_answer_pairs)
+import requests
+import xml.etree.ElementTree
+from raw_xml_data import xml_links
+
+class ClueAnswerPairs:
+    clue_answer_pairs = {}
+
+    # This block iterates through the xml_links list and populates a
+    # dictionary of clue_answer_pairs from all years and all months
+    print('Creating clue answer pair document')
+    for url in xml_links:
+        link_content = requests.get(url).content
+        tree = xml.etree.ElementTree.fromstring(link_content)
+        for child in tree:
+            for child in child:
+                if child.tag == 'Clues':
+                    for child in child:
+                        clue = child.text
+                        ans = child.get('Ans')
+                        clue_answer_pairs[clue] = ans
+
---
+++ raw_xml_data.py
@@ -0,0 +1,55 @@
+## This file parses xwordinfo to ultimately pull a list off all
+## links to xml files, so a link to xml version of puzzles from
+## all available years and all available months
+
+from bs4 import BeautifulSoup
+import requests
+
+class RawXmlDataLinks:
+
+    # This block gets a list called year_links with includes all links to
+    # different years of crossword puzzles
+    def gather_xml_data(link):
+        home = requests.get(link).content
+        soup = BeautifulSoup(home, 'html.parser')
+        home_a_tags = soup.find_all('a')
+        year_links = []
+        month_a_tags = []
+        global xml_links
+        xml_links = []
+
+
+        print('Creating list of links to each year...')
+        for link in home_a_tags:
+            link = link.get('href')
+            if link.startswith('/xml/Crossword/'):
+                link = 'http://www.xwordinfo.com' + link
+                year_links.append(link)
+
+        # This block iterates through year_links and creates a list of
+        # all months links to crossword puzzles
+        print('Creating list of a tags corresponding to each month in each year...')
+        for year in year_links:
+            year_page = requests.get(year).content
+            soup2 = BeautifulSoup(year_page, 'html.parser')
+            month_a_tags.append(soup2.find_all('a'))
+            # Structure: [[1991/02, 1991/03, etc], [1992/01, 1992/03, etc], [etc]]
+
+        # This block iterates through lists month_links and creates a
+        # new list of all links to xml files to be parsed through
+        print('Creating list of links to each xml file for each month in each year')
+        year_count = 1942
+        for set in month_a_tags:
+            for month in set:
+                month = month.get('href')
+                if month.startswith('/xml/Crossword/' + str(1)):
+                    month = 'http://www.xwordinfo.com' + month
+                    xml_links.append(month)
+                elif month.startswith('/xml/Crossword/' + str(2)):
+                    month = 'http://www.xwordinfo.com' + month
+                    xml_links.append(month)
+                else:
+                    print('Added links for ' + str(year_count))
+                    year_count += 1
+
+    gather_xml_data('http://www.xwordinfo.com/xml/Crossword/')
